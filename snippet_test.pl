e31b8051b31dec34c973910731be45bdc81b86e6
Mateus Patricio <mateus.patricio@gmail.com>
2013-02-08 10:00:20 +0000

    Put all

--- snippet_test.pl
+++ snippet_test.pl
@@ -17,3 +17,11 @@
 #
 #--------------------------------------------------------------------------------
 use strict;
+
+my $user        = "user";
+my $password    = "pass";
+my $database    = "db_name";
+my $hostname    = "IP:port";
+my $dsn         = "DBI:mysql:database=$database;host=$hostname";
+my $dbh         = DBI->connect($dsn, $user, $password);
+
